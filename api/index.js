2e0c5c4a8dd74ec34ffd4bd50d9806c026060b17
Ahsan Usman <ahsanusman2015@gmail.com>
2024-06-11 23:04:57 +0500

    created sign up page

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
 import userRouter from "./routes/user.route.js";
-import authRouter from "./routes/auth.route.js"
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
 mongoose
@@ -15,10 +15,20 @@ mongoose
   });
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.errmsg || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
 
 const port = process.env.PORT || 3000;
 
