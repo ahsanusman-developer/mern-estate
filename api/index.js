21565741047d514491fc4b3462bda376aca64386
Ahsan Usman <ahsanusman2015@gmail.com>
2024-06-11 22:54:23 +0500

    Created Sign Up Api Route

--- api/index.js
+++ api/index.js
@@ -1,18 +1,24 @@
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js"
 dotenv.config();
 
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
-    console.log("COnnected to Database");
+    console.log("Connected to Database");
   })
   .catch((err) => {
     console.log(err);
   });
 
 const app = express();
+app.use(express.json())
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
 const port = process.env.PORT || 3000;
 
